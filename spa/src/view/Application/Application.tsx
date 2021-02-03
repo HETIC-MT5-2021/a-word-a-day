4e6cff6792115c8340d6b9d466150d6b546db45a
wyllisMonteiro <wyllismonteiro@gmail.com>
2021-02-03 11:33:31 +0100

    feat: add TranslationItem component

--- spa/src/view/Application/Application.tsx
+++ spa/src/view/Application/Application.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import dayjs from "dayjs"
+
+interface Translation {
+  en: string,
+  fr: string,
+  creation_date: dayjs.Dayjs
+}
+
+const translations : Translation[] = [
+  {
+    en: "cat",
+    fr: "chat",
+    creation_date: dayjs().subtract(2, "day")
+  },
+  {
+    en: "dog",
+    fr: "chien",
+    creation_date: dayjs().subtract(3, "day")
+  },
+  {
+    en: "horse",
+    fr: "cheval",
+    creation_date: dayjs().subtract(1, "day")
+  }
+];
+
+export const TranslationItem: React.FunctionComponent<{translation : Translation}> = ({translation}) => {
+  return <div>{ `${translation.en} - ${translation.fr}` }</div>
+}
 
 export interface Props {}
 
-export const Application: React.FunctionComponent<Props> = () => {
-  return <div>This is your app!</div>;
-};
+export const Application: React.FunctionComponent<Props> = () => (
+  <div>
+    {
+      translations.map((translation) => (
+        <TranslationItem translation={translation} />
+      ))
+    }
+  </div>
+);
