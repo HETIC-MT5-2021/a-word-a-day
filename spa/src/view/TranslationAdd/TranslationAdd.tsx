2b52382c83d0bc3a74a5cd75c987c63637610d53
Hugo-T <hugo.tinghino@gmail.com>
2021-02-25 12:02:49 +0100

    Add errors state

--- spa/src/view/TranslationAdd/TranslationAdd.tsx
+++ spa/src/view/TranslationAdd/TranslationAdd.tsx
@@ -6,12 +6,22 @@ export interface Props {
 }
 
 export const TranslationAdd: React.FunctionComponent<Props> = () => {
-    const [note, setNote] = useState('');
-    const [fr, setFr] = useState('');
-    const [en, setEn] = useState('');
+    const [note, setNote] = useState<string>('');
+    const [fr, setFr] = useState<string>('');
+    const [en, setEn] = useState<string>('');
+    const [enError, setEnError] = useState<boolean>(false);
+    const [frError, setFrError] = useState<boolean>(false);
 
-    const handleChangeFr = (event: React.SyntheticEvent<HTMLInputElement>) => {
-        setFr((event.target as HTMLInputElement).value);
+    const handleChangeFr = (event: React.SyntheticEvent<HTMLInputElement>): void => {
+        const value = (event.target as HTMLInputElement).value
+
+        setFr(value);
+
+        if (value) {
+            return setFrError(false)
+        }
+
+        setFrError(true)
     }
 
     const handleChangeEn = (event: React.SyntheticEvent<HTMLInputElement>) => {
@@ -37,21 +47,24 @@ export const TranslationAdd: React.FunctionComponent<Props> = () => {
     return (
         <div>
             <div className="field">
-                <label className="label">Name</label>
+                <label className="label">Anglais *</label>
                 <div className="control">
                     <input className="input" type="text" placeholder='En' value={en} onChange={handleChangeEn}/>
                 </div>
             </div>
 
             <div className="field">
-                <label className="label">Username</label>
+                <label className="label">Français *</label>
                 <div className="control">
                     <input className="input" type="text" placeholder='Fr' value={fr} onChange={handleChangeFr}/>
                 </div>
+                {frError && (
+                    <p className="help is-danger">Ce champ doit être rempli</p>
+                )}
             </div>
 
             <div className="field">
-                <label className="label">Message</label>
+                <label className="label">Note</label>
                 <div className="control">
                     <textarea className="textarea" placeholder='Note' value={note} onChange={handleChangeNote}/>
                 </div>
