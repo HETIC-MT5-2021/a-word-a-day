6839f23f0ba4770a9a80ca94212fe27c01ebb6ad
Jean-Jacques Akakpo <akakpo.jeanjacques@gmail.com>
2021-02-25 14:21:47 +0100

    🎨 Adds links to router

--- spa/src/index.tsx
+++ spa/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./storage/store";
 import * as serviceWorker from "./serviceWorker";
@@ -16,9 +16,11 @@ ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <Router>
+        <Link to="/translations">Liste</Link>
+        <Link to="/translation/add">Ajouter</Link>
         <Switch>
           <Route exact path="/" component={Application} />
-          <Route exact path="/translation" component={TranslationList} />
+          <Route exact path="/translations" component={TranslationList} />
           <Route exact path="/translation/add" component={TranslationAdd} />
           <Route exact path="/translation/:id" component={TranslationDetails} />
           <Route exact path="/counter" component={Counter} />
